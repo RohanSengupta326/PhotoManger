4533a33a9bec1345abe34a8b3b98bf879a1a5d31
RohanSengupta326 <senguptarohan34@gmail.com>
2022-02-13 16:42:24 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'photoManager',
+      theme: ThemeData(),
+    );
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(),
+    body: Center(),
+  );
+}
